dfd7cf6d6674a5df62b8f26b7be85de267194e9d
Geoffrey Merck <F4FXL@users.noreply.github.com>
2021-12-23 20:53:21 +0100

    Add Thread class

---
+++ Thread.h
@@ -0,0 +1,44 @@
+/*
+ *   Copyright (c) 2021 by Geoffrey Merck F4FXL / KC3FRA
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * A simple class to losely mimic wxthread
+ */
+#ifndef	Thread_H
+#define	Thread_H
+
+#include <thread>
+
+class CThread {
+public:
+    CThread();
+    void Create();
+    void Run();
+    void Wait();
+    void Sleep(unsigned long milli);
+
+protected:
+    virtual void* Entry();
+
+private:
+    static void EntryRunner(CThread * thread);
+
+    std::thread m_thread;
+};
+
+#endif
