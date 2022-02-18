874c3ab7b44e16de57f131f854870cfdf317aa33
Geoffrey Merck <F4FXL@users.noreply.github.com>
2022-02-18 15:37:18 +0100

    #20 add app

---
+++ DGWTimeServer/DGWTimeServerApp.cpp
@@ -0,0 +1,32 @@
+/*
+ *   Copyright (C) 2014 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2022 by Geoffrey Merck F4FXL / KC3FRA
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <string>
+#include <iostream>
+
+#include "DGWTimeServerApp.h"
+
+int main(int argc, char * argv[])
+{
+    if (2 != argc) {
+		printf("usage: %s path_to_config_file\n", argv[0]);
+		printf("       %s --version\n", argv[0]);
+		return 1;
+	}
+}
