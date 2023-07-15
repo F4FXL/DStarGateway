0cbf2010ffba799a7235c2216b7507bc8ccb2b9d
Geoffrey Merck <F4FXL@users.noreply.github.com>
2023-07-15 08:20:52 +0200

    add compatiblitw with libgps 14

--- Common/APRSGPSDIdFrameProvider.cpp
+++ Common/APRSGPSDIdFrameProvider.cpp
@@ -74,13 +74,14 @@ bool CAPRSGPSDIdFrameProvider::buildAPRSFramesInt(const CAPRSEntry * entry, std:
 		return false;
 #endif
 
-#if GPSD_API_MAJOR_VERSION >= 10
+#if GPSD_API_MAJOR_VERSION >= 10 && GPSD_API_MAJOR_VERSION < 14
 	if(m_gpsdData.fix.status == STATUS_NO_FIX)
 		return false;
 #else
-	if (m_gpsdData.status != STATUS_FIX)
+	if (m_gpsdData.fix.status < STATUS_GPS)
 		return false;
 #endif
+
 
 	bool latlonSet   = (m_gpsdData.set & LATLON_SET) == LATLON_SET;
 	bool altitudeSet = (m_gpsdData.set & ALTITUDE_SET) == ALTITUDE_SET;
